31994bc1cedab3ee8d24c76472d7f98a21811110
Mr.Zhang <562123314@qq.com>
2019-06-10 22:02:56 +0800

    基础架构

--- src/test/java/com/cto/project/ProjectApplicationTests.java
+++ src/test/java/com/cto/project/ProjectApplicationTests.java
@@ -1,9 +1,6 @@
 package com.cto.project;
 
-import com.cto.project.entity.User;
-import com.cto.project.service.UserService;
 import com.cto.project.service.redis.RedisCacheService;
-import com.cto.project.utils.UUIDUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,20 +13,10 @@ import java.util.Date;
 @SpringBootTest
 public class ProjectApplicationTests {
     @Autowired
-    private UserService userService;
-    @Autowired
     private RedisCacheService redisCacheService;
 
     @Test
     public void contextLoads() {
-        User user = new User();
-        user.setNickName("aaaa");
-        user.setUserName("bbbb");
-        user.setPassword("1234");
-        user.setCreateTime(new Date());
-        user.setUpdateTime(new Date());
-        redisCacheService.setCacheObject("user1",user);
-//        userService.insert(user);
     }
 
 }
